fc23e9c601c3c847d2c2f6ef08dbb93a164e5399
shona <shonavasanthi2007@gmail.com>
2026-09-05 13:49:46 +0530

    Final syntax cleanups

--- app.py
+++ app.py
@@ -103,6 +103,6 @@ def complete_service(token_id):
     conn.commit()
     conn.close()
     return redirect('/staff')
-if __name__ == '__main__':
+    if __name__ == '__main__':
    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='0.0.0.0', port=port)
